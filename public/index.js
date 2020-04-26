6a688cf1a4d5f35e177a63c05e711724205264c2
Michael Morrison <morrim5@rpi.edu>
2020-04-26 03:49:36 -0500

    .

--- public/index.js
+++ public/index.js
@@ -1,14 +1,46 @@
-$(document).ready(function(){
+function initializeSliders(){
   $("#redslide, #greenslide, #blueslide").slider({
     orientation: "horizontal",
     range: "min",
-    max: 255,
-    value: 127,
-    // slide: showOutput,
+    min: 0,
+    max: 257,
+    step: 1,
+    value: 129,
+    slide: slide
     // change: showOutput
   });
+}
 
-  $("#redslide").slider("value", 150);
-  $("#greenslide").slider("value", 50);
-  $("#blueslide").slider("value", 100);
+function slide(){
+  switch(this.id.replace("slide","")){
+    case "red":
+      $("#slider-swatch-r").css("background-color","rgba(255,0,0,"+$(this).slider('value')/255+")");
+      $("#slider-num-r").html($(this).slider('value') - 1);
+      break;
+    case "green":
+      $("#slider-swatch-g").css("background-color","rgba(0,255,0,"+$(this).slider('value')/255+")");
+      $("#slider-num-g").html($(this).slider('value') - 1);
+      break
+    case "blue":
+      $("#slider-swatch-b").css("background-color","rgba(0,0,255,"+$(this).slider('value')/255+")");
+      $("#slider-num-b").html($(this).slider('value') - 1);
+      break;
+  }
+}
+
+function resetSliders(){
+  $("#redslide").slider("value", 129);
+  $("#greenslide").slider("value", 129);
+  $("#blueslide").slider("value", 129);
+  $("#slider-swatch-r").css("background-color", "rgba(255,0,0,0.5)");
+  $("#slider-swatch-g").css("background-color", "rgba(0,255,0,0.5)");
+  $("#slider-swatch-b").css("background-color", "rgba(0,0,255,0.5)");
+  $("#slider-num-r").html("128");
+  $("#slider-num-g").html("128");
+  $("#slider-num-b").html("128");
+}
+
+$(document).ready(function(){
+  initializeSliders();
+  resetSliders();
 })
